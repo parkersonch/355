08287790610cacdd8baef7efbdad566ea85b8d1b
Charles Parkerson <parkersonch@vcu.edu>
2020-03-06 01:19:35 -0500

    i'm trying to add a login activity i wanted to make a branch bc that's what duke said to do but it wouldn't let me do that; it said i needed to commit first so i'm committing

---
+++ app/src/main/java/vcu/edu/a355teamproject/HomePage.java
@@ -0,0 +1,14 @@
+package vcu.edu.a355teamproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+}
---
+++ app/src/main/java/vcu/edu/a355teamproject/ui/login/LoggedInUserView.java
@@ -0,0 +1,17 @@
+package vcu.edu.a355teamproject.ui.login;
+
+/**
+ * Class exposing authenticated user details to the UI.
+ */
+class LoggedInUserView {
+    private String displayName;
+    //... other data fields that may be accessible to the UI
+
+    LoggedInUserView(String displayName) {
+        this.displayName = displayName;
+    }
+
+    String getDisplayName() {
+        return displayName;
+    }
+}
